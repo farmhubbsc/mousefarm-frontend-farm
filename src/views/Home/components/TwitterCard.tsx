34ae9f0a20c96022b5f656bba63b6a873d5b3072
Toad Farm <toadfarm@protonmail.com>
2021-03-17 23:01:58 +0100

    Updated Twitter URL

--- src/views/Home/components/TwitterCard.tsx
+++ src/views/Home/components/TwitterCard.tsx
@@ -35,7 +35,7 @@ const TwitterCard = () => {
         <Timeline
           dataSource={{
             sourceType: 'profile',
-            screenName: 'MouseFarmBSC'
+            screenName: 'FarmHubBSC'
           }}
           options={{
             height: '300',
